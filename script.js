4f845da60ab7f19452c37b7998512d95019fb012
JoaquinR <jrduran1998@gmail.com>
2025-02-24 13:03:18 -0600

    este commit ya contiene las integraciones de navbar y footer en todos los html

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function enviaAlerta() {
-    const firstName = document.getElementById('first-name').value.trim();
-    const email = document.getElementById('email').value.trim();
-    const message = document.getElementById('message').value.trim();
+  const firstName = document.getElementById("first-name").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const message = document.getElementById("message").value.trim();
 
-    if (firstName === '' || email === '' || message === '') {
-        alert('Por favor, completa todos los campos obligatorios.');
-    } else {
-        alert('Mensaje enviado con éxito.');
-    }
+  if (firstName === "" || email === "" || message === "") {
+    alert("Por favor, completa todos los campos obligatorios.");
+  } else {
+    alert("Mensaje enviado con éxito.");
+  }
 }
